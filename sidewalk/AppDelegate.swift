1cdfc65dc3d08fe800f48a1902c7499e28d83589
Yi-Hua Liang <yihua1218@gmail.com>
2017-09-24 17:58:14 +0800

    Path log.

--- sidewalk/AppDelegate.swift
+++ sidewalk/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyBbd1P9vmp5C8PD2my7UaCKkCuvD1H5tDE")
+        GMSServices.provideAPIKey("AIzaSyCRoA9ugcAFnaS3GH8kWnNM2rUyeWQV-wU")
         return true
     }
 
--- sidewalk/ViewController.swift
+++ sidewalk/ViewController.swift
@@ -17,28 +17,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
     private var renderer: GMUGeometryRenderer!
     private var kmlParser: GMUKMLParser!
     
+    // Locations
     var locationManager = CLLocationManager()
     var currentLocation: CLLocation?
+    var lastLocation: CLLocation?
+    
     var mapView: GMSMapView!
     var zoomLevel: Float = 21.0
-    let path = GMSMutablePath()
+    var path : GMSMutablePath?
     var polyline : GMSPolyline!
+    var state = true
     var start = false
     
     // Panel
     var recordButton : UIButton!
     var recordImage : UIImage!
     var recordImageHighlighted : UIImage!
+
     var pauseButton : UIButton!
     var pauseImage : UIImage!
     var pauseImageHighlighted : UIImage!
+
     var stopButton : UIButton!
     var stopImage : UIImage!
     var stopImageHighlighted : UIImage!
-    
+
+    var stateButton : UIButton!
+    var okImage : UIImage!
+    var okImageHighlighted : UIImage!
+    var blockImage : UIImage!
+    var blockImageHighlighted : UIImage!
+
+    var triangleButton : UIButton!
+    var triangleImage : UIImage!
+    var triangleImageHighlighted : UIImage!
+
     // Color
     let green = UIColor.init(red: 15.0/255.0, green: 157.0/255.0, blue: 88.0/255.0, alpha: 1.0)
     let red = UIColor.init(red: 165.0/255.0, green: 39.0/255.0, blue: 20.0/255.0, alpha: 1.0)
+    let yellow = UIColor.init(red: 255.0/255.0, green: 214.0/255.0, blue: 0.0/255.0, alpha: 1.0)
     var color = UIColor.init(red: 15.0/255.0, green: 157.0/255.0, blue: 88.0/255.0, alpha: 1.0)
 
     override func viewDidLoad() {
@@ -50,6 +67,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
         locationManager.delegate = self;
+        locationManager.allowsBackgroundLocationUpdates = true
         
         let camera = GMSCameraPosition.camera(withLatitude: 25.0491699, longitude: 121.509634, zoom: zoomLevel)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
@@ -81,25 +99,50 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
         renderer.render()
         
         // Panel
+        recordImage = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FACircle, topTextColor: .red, bgLarge: true)
+        recordImageHighlighted = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FACircle, topTextColor: .white, bgLarge: true)
         pauseImage = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FAPause, topTextColor: .white, bgLarge: true)
         pauseImageHighlighted = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FAPause, topTextColor: .gray, bgLarge: true)
         pauseButton = UIButton()
-        pauseButton.setImage(pauseImage, for: .normal)
-        pauseButton.setImage(pauseImageHighlighted, for: .highlighted)
+        pauseButton.setImage(recordImage, for: .normal)
+        pauseButton.setImage(recordImageHighlighted, for: .highlighted)
         view.addSubview(pauseButton)
         pauseButton.autoPinEdge(toSuperviewEdge: ALEdge.bottom, withInset: 10.0)
         pauseButton.autoAlignAxis(toSuperviewAxis: ALAxis.vertical)
+        pauseButton.addTarget(self, action: #selector(pauseButtonClick), for: .touchDown)
         
-        recordImage = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FACircle, topTextColor: .red, bgLarge: true)
-        recordImageHighlighted = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FACircle, topTextColor: .white, bgLarge: true)
         recordButton = UIButton()
         recordButton.setImage(recordImage, for: .normal)
         recordButton.setImage(recordImageHighlighted, for: .highlighted)
-        view.addSubview(recordButton)
-        recordButton.autoPinEdge(ALEdge.right, to: ALEdge.left, of: pauseButton, withOffset: -10)
-        recordButton.autoAlignAxis(ALAxis.horizontal, toSameAxisOf: pauseButton)
-        recordButton.addTarget(self, action: #selector(recordButtonClick), for: .touchDown)
-        
+        if (false) {
+            view.addSubview(recordButton)
+            recordButton.autoPinEdge(ALEdge.right, to: ALEdge.left, of: pauseButton, withOffset: -10)
+            recordButton.autoAlignAxis(ALAxis.horizontal, toSameAxisOf: pauseButton)
+            recordButton.addTarget(self, action: #selector(recordButtonClick), for: .touchDown)
+        }
+
+        okImage = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FACheck, topTextColor: green, bgLarge: true)
+        okImageHighlighted = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FACheck, topTextColor: .white, bgLarge: true)
+        blockImage = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FABan, topTextColor: red, bgLarge: true)
+        blockImageHighlighted = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FABan, topTextColor: .white, bgLarge: true)
+        stateButton = UIButton()
+        stateButton.setImage(okImage, for: .normal)
+        stateButton.setImage(okImageHighlighted, for: .highlighted)
+        view.addSubview(stateButton)
+        stateButton.autoPinEdge(ALEdge.right, to: ALEdge.left, of: pauseButton, withOffset: -10)
+        stateButton.autoAlignAxis(ALAxis.horizontal, toSameAxisOf: pauseButton)
+        stateButton.addTarget(self, action: #selector(stateButtonClick), for: .touchDown)
+
+        triangleImage = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FAExclamationTriangle, topTextColor: yellow, bgLarge: true)
+        triangleImageHighlighted = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FAExclamationTriangle, topTextColor: .white, bgLarge: true)
+        triangleButton = UIButton()
+        triangleButton.setImage(triangleImage, for: .normal)
+        triangleButton.setImage(triangleImageHighlighted, for: .highlighted)
+        view.addSubview(triangleButton)
+        triangleButton.autoPinEdge(ALEdge.right, to: ALEdge.left, of: stateButton, withOffset: -10)
+        triangleButton.autoAlignAxis(ALAxis.horizontal, toSameAxisOf: pauseButton)
+        triangleButton.addTarget(self, action: #selector(triangleButtonClick), for: .touchDown)
+
         stopImage = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FAStop, topTextColor: .white, bgLarge: true)
         stopImageHighlighted = UIImage.init(bgIcon: .FACircle, bgTextColor: .black, topIcon: .FAStop, topTextColor: .gray, bgLarge: true)
         stopButton = UIButton()
@@ -108,6 +151,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
         view.addSubview(stopButton)
         stopButton.autoPinEdge(ALEdge.left, to: ALEdge.right, of: pauseButton, withOffset: 10.0)
         stopButton.autoAlignAxis(ALAxis.horizontal, toSameAxisOf: pauseButton)
+        
+        // Callback for entring background
+        NotificationCenter.default.addObserver(self, selector: #selector(willResignActive), name: .UIApplicationWillResignActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: .UIApplicationWillEnterForeground, object: nil)
     }
     
     override func didReceiveMemoryWarning() {
@@ -117,13 +164,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location: CLLocation = locations.last!
+        var distance = 0.0
         print("Location: \(location)")
         
-        if (start) {
-            path.add(CLLocationCoordinate2D(latitude:location.coordinate.latitude, longitude: location.coordinate.longitude))
-            polyline = GMSPolyline(path: path)
+        if (lastLocation != nil) {
+            distance = location.distance(from: lastLocation!)
+            print("distance: \(distance)")
+        }
+        
+        lastLocation = location
+        
+        if (path != nil && distance > 1.0) {
+            path?.add(CLLocationCoordinate2D(latitude:location.coordinate.latitude, longitude: location.coordinate.longitude))
+            polyline.path = path
             polyline.strokeWidth = 10
-            polyline.strokeColor = green
+            polyline.strokeColor = color
             polyline.map = mapView
         }
         
@@ -165,6 +220,70 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
     @objc func recordButtonClick(sender: UIButton) {
         print("recordButtonClick")
         start = true
+    }
+    
+    @objc func triangleButtonClick(sender: UIButton) {
+        print("triangleButtonClick")
+        if (lastLocation != nil) {
+            let position = CLLocationCoordinate2D(latitude:(lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!)
+            let marker = GMSMarker(position: position)
+            marker.icon = UIImage.init(bgIcon: .FACircle, bgTextColor: UIColor(white: 1, alpha: 0.0), topIcon: .FAExclamationTriangle, topTextColor: yellow, bgLarge: true)
+            marker.map = mapView
+        }
+    }
+
+    @objc func stateButtonClick(sender: UIButton) {
+        print("stateButtonClick")
+        if (state == false) {
+            state = true
+            color = green
+            path = GMSMutablePath()
+            path?.add(CLLocationCoordinate2D(latitude:(lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!))
+            polyline = GMSPolyline(path: path)
+            stateButton.setImage(okImage, for: .normal)
+            stateButton.setImage(okImageHighlighted, for: .highlighted)
+        } else {
+            state = false
+            color = red
+            path = GMSMutablePath()
+            path?.add(CLLocationCoordinate2D(latitude:(lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!))
+            polyline = GMSPolyline(path: path)
+            stateButton.setImage(blockImage, for: .normal)
+            stateButton.setImage(blockImageHighlighted, for: .highlighted)
+        }
+    }
+
+    @objc func pauseButtonClick(sender: UIButton) {
+        print("pauseButtonClick")
+        
+        if (start == false) {
+            start = true
+            path = GMSMutablePath()
+            polyline = GMSPolyline(path: path)
+            pauseButton.setImage(pauseImage, for: .normal)
+            pauseButton.setImage(pauseImageHighlighted, for: .highlighted)
+        } else {
+            start = false
+            pauseButton.setImage(recordImage, for: .normal)
+            pauseButton.setImage(recordImageHighlighted, for: .highlighted)
+            path = nil
+        }
+    }
+    
+    @objc func willResignActive(sender: UIButton) {
+        print("willResignActive")
+        if (start == true) {
+            print("background location update")
+        } else {
+            print("stop location update")
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    @objc func willEnterForeground(sender: UIButton) {
+        print("willEnterForeground")
+        print("startUpdatingLocation")
+        locationManager.startUpdatingLocation()
     }
 }
 
